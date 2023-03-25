0394070840ca46dc7ded16df7949541eaa322d17
Manoel Duran <manoel.duran@hotmail.com>
2023-03-25 18:43:43 -0300

    feat(puzzlesJson): created fake database

---
+++ puzzles.ts
@@ -0,0 +1,75 @@
+
+export const puzzles = [
+    {
+        id: 0,
+        message: "Agora que você topou esse desafio, vamos começar a caça ao tesouro, seu primeiro enigma é: ... "
+    },
+    {
+        id: 1,
+        message: "Parabéns! Parece que você conseguiu resolver seu primeiro enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 2,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 3,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 4,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 5,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 6,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 7,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 8,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 9,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 10,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 11,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 12,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 13,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 14,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 15,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 16,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    },
+    {
+        id: 17,
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+    }
+]
