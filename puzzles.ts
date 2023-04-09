474555ba9f7889486c39e63afe052ea2e88109bf
Manoel Duran <manoel.duran@hotmail.com>
2023-04-08 21:47:13 -0300

    fixed puzzles

--- puzzles.ts
+++ puzzles.ts
@@ -2,74 +2,46 @@
 export const puzzles = [
     {
         id: 0,
-        message: "Agora que você topou esse desafio, vamos começar a caça ao tesouro, seu primeiro enigma é: ... "
+        message: "Agora que você topou esse desafio, vamos começar a caça ao tesouro, seu primeiro enigma é: Eu estou em um local que você vai cansada e sai quase sempre renovada, mas cuidado com seus peitos, neste local, um gato esperto costuma ficar subindo em você, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 1,
-        message: "Parabéns! Parece que você conseguiu resolver seu primeiro enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver seu primeiro enigma, então vamos para mais um: Eu estou no lugar mais usado desta casa, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 2,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou no antigo lugar preferido de sua filha, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 3,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou em um lugar onde pessoas me utilizam para praticar esportes, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 4,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou na prisão, provavelmente eu deva ter feito algo de errado...boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 5,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou ... que cheiro bom de calabresa, parece que é recheada, fui,boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 6,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou... parece que é um cemitério... alguem aqui toma muito café, essa pessoa tem bom gosto, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 7,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou em um lugar que sua filha ama, porém bem fedorento, ela deve ter algum problema, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 8,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou onde você vai e manu quer ir junto, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 9,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: Eu estou... um que cheirinho bom, esses produtos da Glade são muito bons. (Propaganda não remunerada) Estou em um lugar que você enche de tralha e esquece de tirar, boa sorte! Caso você me encontre, digite `Achei`, para avançarmos no nosso desafio."
     },
     {
         id: 10,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
+        message: "Parabéns! Parece que você conseguiu concluir todos os enigmas. Feliz Páscoa! By: Manoel Duran"
     },
-    {
-        id: 11,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 12,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 13,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 14,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 15,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 16,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    },
-    {
-        id: 17,
-        message: "Parabéns! Parece que você conseguiu resolver mais um enigma, então vamos para mais um: ... "
-    }
 ]
--- src/modules/Sender/Sender.ts
+++ src/modules/Sender/Sender.ts
@@ -16,22 +16,23 @@ class Sender {
     this.client = client;
     await this.client.onMessage(async (message: Message) => {
       console.log('message', message)
+      if (message.chatId === process.env.chatId && message.body === 'Sim') {
+          const selectedMessage = puzzles[0].message
+          await this.sendText(process.env.chatId, selectedMessage)
+          index += 1
+      }
       if (message.chatId === process.env.chatId && message.body === 'Achei') {
-        try {
           console.log('index', index)
           const selectedMessage = puzzles[index].message
           await this.sendText(process.env.chatId, selectedMessage)
           index += 1
-        } catch (err) {
-          console.log('error', err)
-        }
       }
     });
   }
   private initialize() {
     const start = (client: Whatsapp) => {
       this.client = client;
-      this.sendText(process.env.phone, "Ola, tudo bem? esse é um teste")
+      this.sendText(process.env.chatId, "Ola, Brenda, tudo bem? Temos um desafio para concluir e precisamos de sua ajuda, nos ajude a encontrar os 10 chocolates perdidos. Responda `Sim`, para iniciar!")
     }
     create({
       session: 'treasure-hunt',
